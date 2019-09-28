70c79eb20645f77de267dcfdc89b235d37d58d9b
Vamsi <vamsidhar2mp@gmail.com>
2019-09-28 19:31:42 -0400

    Version 1.0 deployed

--- Portfoilio/main.py
+++ Portfoilio/main.py
@@ -1,12 +1,27 @@
 from flask import Flask
 from flask import render_template
+from flask import send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('user.html')
+    return render_template('index.html')
+
+@app.route('/resume')
+def show_resume():
+    return render_template('resume.html')
 
 @app.route('/projects')
-def projects():
-    return render_template('projects.html')
+def show_projects():
+    return render_template('portfolio.html')
+
+@app.route('/contact')
+def show_contact():
+    return render_template('contact.html')
+
+
+@app.route('/download')
+def downloadFile ():
+    path = "static/resume.pdf"
+    return send_file(path, as_attachment=True)
